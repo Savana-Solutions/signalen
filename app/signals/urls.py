47b1ca492c5a3a046aefa206069a761ee228b977
MichaelSchaapDev <michael_schaap@outlook.com>
2024-02-26 13:40:35 +0100

    chore: security.txt

--- app/signals/urls.py
+++ app/signals/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from signals.apps.api.generics.routers import BaseSignalsAPIRootView
@@ -16,29 +15,17 @@ admin.site.site_url = None
 urlpatterns = [
     # Used to determine API health when deploying
     path('status/', include('signals.apps.health.urls')),
-
-    # URL pattern for robots.txt
-    path(
-        "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
-    ),
-
-    # URL pattern for security.txt
-    path(
-        "security.txt",
-        TemplateView.as_view(template_name="security.txt", content_type="text/plain"),
-    ),
     
     # The Signals application is routed behind the HAproxy with `/signals/` as path.
-    path('signals/', BaseSignalsAPIRootView.as_view()),
-    path('signals/', include('signals.apps.api.urls')),
+    path('', BaseSignalsAPIRootView.as_view()),
+    path('', include('signals.apps.api.urls')),
 
     # The Django admin
-    path('signals/admin/', admin.site.urls),
-    re_path(r'^signals/markdownx/', include('markdownx.urls')),
+    path('admin/', admin.site.urls),
+    re_path(r'^markdownx/', include('markdownx.urls')),
 
     # SOAP stand-in endpoints
-    path('signals/sigmax/', include('signals.apps.sigmax.urls')),
+    path('sigmax/', include('signals.apps.sigmax.urls')),
 ]
 
 if settings.DEBUG:
@@ -49,8 +36,8 @@ if settings.DEBUG:
 
 if settings.OIDC_RP_CLIENT_ID:
     urlpatterns += [
-        path('signals/oidc/login_failure/', TemplateView.as_view(template_name='admin/oidc/login_failure.html')),
-        path('signals/oidc/', include('mozilla_django_oidc.urls')),
+        path('oidc/login_failure/', TemplateView.as_view(template_name='admin/oidc/login_failure.html')),
+        path('oidc/', include('mozilla_django_oidc.urls')),
     ]
 
 if settings.SILK_ENABLED:
@@ -58,6 +45,6 @@ if settings.SILK_ENABLED:
 
 # DRF Spectacular
 urlpatterns += [
-    path('signals/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('signals/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
