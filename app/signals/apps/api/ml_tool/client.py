6594282befe8e745707bbc55982a8dd4ba9e93f6
MichaelSchaapDev <michael_schaap@outlook.com>
2024-09-05 16:37:47 +0200

    fix: client

--- app/signals/apps/api/ml_tool/client.py
+++ app/signals/apps/api/ml_tool/client.py
@@ -1,18 +1,13 @@
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
 import json
-
 import requests
 from django.conf import settings
 from django.core import validators
 from requests import ConnectTimeout
-
 from signals.apps.api.generics.exceptions import GatewayTimeoutException
-
 
 class MLToolClient:
     timeout = (10.0, 10.0)
-    endpoint = '{}/predict'.format(settings.ML_TOOL_ENDPOINT)
+    endpoint = "{}/predict".format(settings.ML_TOOL_ENDPOINT)
     predict_validators = [
         validators.MinLengthValidator(limit_value=1),
     ]
@@ -22,9 +17,12 @@ class MLToolClient:
             validator(text)
 
         try:
-            data = json.dumps({'text': text})
-            response = requests.post(self.endpoint, data=data, timeout=self.timeout)
-        except (ConnectTimeout, ):
+            data = json.dumps({"text": text})
+            headers = {"Content-Type": "application/json"}
+            response = requests.post(
+                self.endpoint, data=data, headers=headers, timeout=self.timeout
+            )
+        except ConnectTimeout:
             raise GatewayTimeoutException()
         else:
             return response
