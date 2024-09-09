670f53b8fa45e74031c7d9216f4103d0cbb3c666
MichaelSchaapDev <michael_schaap@outlook.com>
2024-09-09 12:27:25 +0200

    fix: migrations

--- app/signals/apps/signals/migrations/0028_auto_20181204_0915.py
+++ app/signals/apps/signals/migrations/0028_auto_20181204_0915.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('signals', '0027_SIG858'),
+        ('signals', '0026_history'),
     ]
 
     operations = [
