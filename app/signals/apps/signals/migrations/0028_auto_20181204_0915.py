ee62bdd4c35d4efe663985e8b94e97e30cbe0772
MichaelSchaapDev <michael_schaap@outlook.com>
2024-09-09 13:24:06 +0200

    Revert "fix: migrations"
    This reverts commit 670f53b8fa45e74031c7d9216f4103d0cbb3c666.

--- app/signals/apps/signals/migrations/0028_auto_20181204_0915.py
+++ app/signals/apps/signals/migrations/0028_auto_20181204_0915.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('signals', '0026_history'),
+        ('signals', '0027_SIG858'),
     ]
 
     operations = [
