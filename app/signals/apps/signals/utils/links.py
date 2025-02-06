1c4428100874f17c5c90e33ad9c905ad57ad82f7
MichaelSchaapDev <michael_schaap@outlook.com>
2025-02-06 09:22:24 +0100

    chore: uuid & pk in url

--- app/signals/apps/signals/utils/links.py
+++ app/signals/apps/signals/utils/links.py
@@ -5,7 +5,11 @@ class HTTPSLinksField(LinksField):
     def get_url(self, obj, view_name, request, format):
         kwargs = getattr(self, 'kwargs', None)
         if kwargs is None:
-            kwargs = {'pk': obj.pk} if hasattr(obj, 'pk') else {}
+            # If the view name contains 'public' and the object has a uuid, use that
+            if 'public' in view_name and hasattr(obj, 'uuid'):
+                kwargs = {'uuid': obj.uuid}
+            else:
+                kwargs = {'pk': obj.pk} if hasattr(obj, 'pk') else {}
 
         url = reverse(view_name, kwargs=kwargs, request=request, format=format)
         
